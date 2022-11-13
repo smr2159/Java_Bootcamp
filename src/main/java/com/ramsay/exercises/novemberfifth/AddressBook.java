ac69cdc20eac665ae79c21f7cf80f5628cb8b369
sharon <sharon@example.com>
2022-11-13 12:12:02 -0500

    These are my Java bootcamp packages and files as of 10/13/2022.

--- src/main/java/com/ramsay/exercises/novemberfifth/AddressBook.java
+++ src/main/java/com/ramsay/exercises/novemberfifth/AddressBook.java
@@ -1,12 +1,27 @@
 package com.ramsay.exercises.novemberfifth;
 
+import java.util.ArrayList;
+
 public class AddressBook {
+    public static void main (String [] args) {
 
-    public void addContact () {
-
-    }
-
-    public void findContact () {
 
     }
+
+    public ArrayList addContact (String contact) {
+        ArrayList <String> addressBook = new ArrayList<>();
+        addressBook.add(contact);
+        return addressBook;
+    }
+
+    public String findContact (String lastName, ArrayList addressBook) {
+        for (String line : addressBook) {
+            String[] contact = line.split(",");
+            String lastName = addressBook[1];
+            if (addressBook.get(lastName) = lastName) {
+                return line;
+            }
+
+    }
+        return line;
 }
